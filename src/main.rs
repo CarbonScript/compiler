0baf3b79800f826229729522ee8e32a287b6f619
Edward Zhang <643431636@qq.com>
2022-07-25 19:55:12 +0800

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs::File, io::Read};
+
+fn load_source_file(path: &str) -> String {
+    let mut file: File = std::fs::File::open(path).unwrap();
+    let mut contents: String = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    return contents;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents:String = load_source_file("./test/example.carb");
+    print!("{}", contents);
 }
